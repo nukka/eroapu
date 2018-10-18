cc4ca8d24ca9eb864f0b3d24815879e6648b6710
Jenni Törmälä <tormalajenni@gmail.com>
2018-10-18 16:46:43 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Navbar, Nav, NavItem, Jumbotron} from 'react-bootstrap'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+
+                <Navbar collapseOnSelect>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">Eroapua</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle/>
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav>
+                            <NavItem eventKey={1} href="#">
+                                Aihe 1
+                            </NavItem>
+                            <NavItem eventKey={2} href="#">
+                                Aihe 2
+                            </NavItem>
+                            <NavItem eventKey={3} href="#">
+                                Aihe 3
+                            </NavItem>
+                        </Nav>
+                        <Nav pullRight>
+                            <NavItem eventKey={4} href="#">
+                                Yhteystiedot
+                            </NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Jumbotron>
+                    <h1>Eroapua verkossa</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquid ex ea commodi consequat.
+                    </p>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
 
 export default App;
