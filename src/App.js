1da7d4347c47d4a52634d0f84cf2642a4bf885f6
Annukka <tormala.annukka@gmail.com>
2018-11-05 14:40:00 +0200

    Add search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import {FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
 import './App.css';
-import {Navbar, Nav, NavItem, Jumbotron, Col, Grid, Row} from 'react-bootstrap'
+import {Col, Grid, Row} from 'react-bootstrap'
 
 class App extends Component {
     render() {
@@ -9,6 +10,15 @@ class App extends Component {
 
                 <div className="portal-content">
                     <Grid>
+                        <Row>
+                            <Col md={4}>
+                            </Col>
+                            <Col md={4}>
+                                <Hakupalkki/>
+                            </Col>
+                            <Col md={4}>
+                            </Col>
+                        </Row>
                         <Row className="show-grid">
                             <Col md={3}>Haku</Col>
                             <Col md={3}>Aikajana</Col>
@@ -21,6 +31,42 @@ class App extends Component {
             </div>
         );
     }
+}
+
+class Hakupalkki extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            value: ''
+        };
+
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+        //console.log(e.target.value);
+
+    }
+
+    render() {
+        return (
+            <form>
+                <FormGroup controlId="searchBar">
+                    <ControlLabel>Vapaa haku</ControlLabel>
+                    <FormControl
+                        type="text"
+                        value={this.state.value}
+                        placeholder="Syötä haluamasi hakusana"
+                        onChange={this.handleChange}
+                    />
+
+                </FormGroup>
+            </form>
+        );
+    }
+
 }
 
 export default App;
