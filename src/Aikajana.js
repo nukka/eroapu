330ed04c1d5eae948294f05bc27749ce00cffcc9
jenniT <tormalajenni@gmail.com>
2018-11-12 16:37:25 +0200

    Add fetching database data via API

--- src/Aikajana.js
+++ src/Aikajana.js
@@ -4,58 +4,44 @@ import 'react-vertical-timeline-component/style.min.css';
 import {Jumbotron} from "react-bootstrap";
 
 class Aikajana extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {testarray: []};
+        fetch('http://localhost:3001/test')
+            .then(response => response.json())
+            .then(testarray => (this.setState({testarray})))
+    }
+
     render() {
+
+        console.log("test array: " + JSON.stringify(this.state.testarray));
+
         return (
             <div className="timeline">
                 <Jumbotron className="otsikko">
                     <h1>Aikajana</h1>
-                    <div className="helptext" sm={1} md={4}> Alle on listattu eron eri vaiheet. Lorem ipsum dolor sit amet, no dicit maiorum
+                    <div className="helptext" sm={1} md={4}> Alle on listattu eron eri vaiheet. Lorem ipsum dolor sit
+                        amet, no dicit maiorum
                         appetere est, pro te hinc quas verear.
                     </div>
                 </Jumbotron>
 
-                <VerticalTimeline>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
-                    >
-                        <h3 className="vertical-timeline-element-title">Eron aihe 1</h3>
-                        <p>
-                            Lorem ipsum <a href="#"> <br/> linkki </a>
-                        </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
-                    >
-                        <h3 className="vertical-timeline-element-title">Eron vaihe 2 </h3>
-                        <h4 className="vertical-timeline-element-subtitle">Kuvaus</h4>
-                        <p>
-                            Lorem ipsum
-                        </p>
-                    </VerticalTimelineElement>
+                <VerticalTimeline>{this.state.testarray.map(function (item) {
+                    return (
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
+                        >
+                            <h3 className="vertical-timeline-element-title">{item.title}</h3>
+                            <p>
+                                Lorem ipsum <a href={item.link}> <br/> {item.source} </a>
+                            </p>
+                        </VerticalTimelineElement>
+                    )
 
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
-                    >
-                        <h3 className="vertical-timeline-element-title">Eron vaihe 3 </h3>
-                        <h4 className="vertical-timeline-element-subtitle">Kuvaus</h4>
-                        <p>
-                            User Experience, Visual Design
-                        </p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--education"
-                        iconStyle={{background: 'rgb(233, 30, 99)', color: '#fff'}}
-                    >
-                        <h3 className="vertical-timeline-element-title"> Eron vaihe 4</h3>
-                        <h4 className="vertical-timeline-element-subtitle">Kuvaus</h4>
-                        <p>
-                            Lorem ipsum
-                        </p>
-                    </VerticalTimelineElement>
-                </VerticalTimeline>
+                })}</VerticalTimeline>
+
             </div>
         );
     }
