6a866cf5148797a8f5393dc18cee359395ad2cde
jenniT <tormalajenni@gmail.com>
2018-11-14 19:31:21 +0200

    Modify database connections for timeline page

--- src/Aikajana.js
+++ src/Aikajana.js
@@ -1,21 +1,56 @@
 import React, {Component} from 'react';
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import {Jumbotron} from "react-bootstrap";
+import {Jumbotron, Glyphicon} from "react-bootstrap";
 
 class Aikajana extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {testarray: []};
-        fetch('http://localhost:3001/api/aikajana')
+        this.state = {
+            show1stContent: false,
+            show2ndContent: false,
+            harkitsen: [],
+            eronnut: []
+        };
+
+        fetch('http://localhost:3001/api/aikajana/harkitsen_eroa')
             .then(response => response.json())
-            .then(testarray => (this.setState({testarray})))
+            .then(harkitsen => (this.setState({harkitsen})));
+
+        fetch('http://localhost:3001/api/aikajana/olen_eronnut')
+            .then(response => response.json())
+            .then(eronnut => (this.setState({eronnut})));
+    }
+
+    toggleTimeLine1stElement() {
+        console.log("showcontent1: " + this.state.show1stContent);
+        this.setState({show1stContent: !this.state.show1stContent});
+    }
+
+    toggleTimeLine2ndElement (){
+        console.log("showcontent2: " + this.state.show2ndContent);
+        this.setState({show2ndContent: !this.state.show2ndContent});
     }
 
     render() {
+        let divorcedContent = this.state.harkitsen.map(function (item) {
+            return (
+                <p>
+                    {item.title} <a href={item.link}> <br/> {item.source} </a>
+                </p>
 
-        console.log("test array: " + JSON.stringify(this.state.testarray));
+            );
+        });
+
+        let consideringContent = this.state.eronnut.map(function (item) {
+            return (
+                <p>
+                    {item.title} <a href={item.link}> <br/> {item.source} </a>
+                </p>
+
+            );
+        });
 
         return (
             <div className="timeline">
@@ -27,20 +62,32 @@ class Aikajana extends Component {
                     </div>
                 </Jumbotron>
 
-                <VerticalTimeline>{this.state.testarray.map(function (item) {
-                    return (
-                        <VerticalTimelineElement
-                            className="vertical-timeline-element--work"
-                            iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
-                        >
-                            <h3 className="vertical-timeline-element-title">{item.title}</h3>
-                            <p>
-                                Lorem ipsum <a href={item.link}> <br/> {item.source} </a>
-                            </p>
-                        </VerticalTimelineElement>
-                    )
+                <VerticalTimeline>
 
-                })}</VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        iconStyle={{background: 'rgb(33, 150, 243)', color: '#fff'}}
+                    >
+                        <div onClick={() => this.toggleTimeLine1stElement()}>
+                            <h3 className="vertical-timeline-element-title">Harkitsen eroa</h3>
+                            <Glyphicon className="pull-right" glyph="glyphicon glyphicon-menu-down"/>
+                            {this.state.show1stContent && consideringContent}
+                        </div>
+                    </VerticalTimelineElement>
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        iconStyle={{background: 'rgb(255,105,180)', color: '#fff'}}
+                    >
+                        <div onClick={() => this.toggleTimeLine2ndElement()}>
+                            <h3 className="vertical-timeline-element-title">Olen eronnut</h3>
+                            <div className="down-arrow">
+                            <Glyphicon className="down-arrow pull-right" glyph="glyphicon glyphicon-menu-down"/></div>
+                            {this.state.show2ndContent && divorcedContent}
+                        </div>
+
+                    </VerticalTimelineElement></VerticalTimeline>
+
 
             </div>
         );
--- src/connection.js
+++ src/connection.js
@@ -11,37 +11,55 @@ var con = mysql.createConnection({
     database: "mysql"
 });
 
-con.connect(function (err) {
-    if (err) throw err;
-    con.query("SELECT * FROM mysql.harkitsen_eroa", function (err, result) {
-        if (err) throw err;
-    });
-});
-
 application.use(cors());
 
-application.get('/api/aikajana', function (req, res) {
+application.get('/api/aikajana/harkitsen_eroa', function (req, res) {
 
 
-    con.query('CREATE TABLE IF NOT EXISTS aikajana (title VARCHAR(255), link VARCHAR(255), source VARCHAR(255));', function (error, results) {
+    con.query('CREATE TABLE IF NOT EXISTS harkitsen (title VARCHAR(255), link VARCHAR(255), source VARCHAR(255), phase VARCHAR(255));', function (error, results) {
         if (error) throw error;
     });
 
-    con.query('TRUNCATE TABLE aikajana', function (error, results) {
+    con.query('TRUNCATE TABLE harkitsen', function (error, results) {
         if (error) throw error;
     });
 
-    con.query('LOAD DATA LOCAL INFILE \'csv/harkitsen.csv\' INTO TABLE aikajana FIELDS TERMINATED BY \',\' ENCLOSED BY \'"\' LINES TERMINATED BY \'\\n\' IGNORE 1 ROWS (title, link, source);', function (error, results) {
+    con.query('LOAD DATA LOCAL INFILE \'csv/harkitsen.csv\' INTO TABLE harkitsen FIELDS TERMINATED BY \',\' ENCLOSED BY \'"\' LINES TERMINATED BY \'\\n\' IGNORE 1 ROWS (title, link, source, phase)', function (error, results) {
         if (error) throw error;
     });
 
-    con.query('SELECT * FROM mysql.aikajana;', function (error, results) {
+
+    con.query('SELECT * FROM mysql.harkitsen;', function (error, results) {
         if (error) throw error;
         res.send(results)
     });
 
 });
 
+application.get('/api/aikajana/olen_eronnut', function (req, res) {
+
+
+    con.query('CREATE TABLE IF NOT EXISTS eronnut (title VARCHAR(255), link VARCHAR(255), source VARCHAR(255), phase VARCHAR(255));', function (error, results) {
+        if (error) throw error;
+    });
+
+    con.query('TRUNCATE TABLE eronnut', function (error, results) {
+        if (error) throw error;
+    });
+
+    con.query('LOAD DATA LOCAL INFILE \'csv/olen_eronnut.csv\' INTO TABLE eronnut FIELDS TERMINATED BY \',\' ENCLOSED BY \'"\' LINES TERMINATED BY \'\\n\' IGNORE 1 ROWS (@dummy, title, link, source, phase);', function (error, results) {
+        if (error) throw error;
+    });
+
+    con.query('SELECT * FROM mysql.eronnut;', function (error, results) {
+        if (error) throw error;
+        res.send(results)
+    });
+
+});
+
+
 application.listen(3001, () => {
-    console.log('Go to http://localhost:3001/api/aikajana');
+    console.log('Go to http://localhost:3001/api/aikajana/harkitsen_eroa');
+    console.log('Go to http://localhost:3001/api/aikajana/olen_eronnut');
 });
